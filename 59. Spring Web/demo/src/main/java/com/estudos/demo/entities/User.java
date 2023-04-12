e1d2832a37a1c6a3982d67c19faffad2b7073bb9
Ederson Junior <ed.esmeraldino7@gmail.com>
2023-04-12 13:28:55 -0300

    User entity and resource

---
+++ 59. Spring Web/demo/src/main/java/com/estudos/demo/entities/User.java
@@ -0,0 +1,90 @@
+package com.estudos.demo.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id!= null? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id!= null) || (this.id!= null &&!this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+}
